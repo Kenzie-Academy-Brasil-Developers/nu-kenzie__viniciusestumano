3bf9b81e4a3b903d8db2ca01a01a08d0bc814318
root <vsestumano@gmail.com>
2023-10-02 08:23:30 -0300

    feat: text add

--- src/components/FinanceList/index.jsx
+++ src/components/FinanceList/index.jsx
@@ -5,7 +5,7 @@ import style from "./index.module.scss"
 export const FinanceList = ({ listForm, id, removeInfo }) => {
     return (
         <section className={style.list__container}>
-            <h2 className="title2">Resumo financeiro</h2>
+            <h2 className="title3">Resumo financeiro</h2>
             {listForm.length > 0 ? (
             <ul>
                 {listForm.map((info) => {
@@ -14,7 +14,7 @@ export const FinanceList = ({ listForm, id, removeInfo }) => {
                 })}
             </ul>
             ) : 
-            <p className="title3">Você ainda não possui nenhum lançamento</p>}
+            <p className="title2">Você ainda não possui nenhum lançamento</p>}
         </section>
     )
 }
