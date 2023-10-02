e81a2b88130f8cd29934b6c4d02149ab751ed609
root <vsestumano@gmail.com>
2023-10-02 08:20:52 -0300

    feat: additional ternary condition

--- src/components/FinanceList/index.jsx
+++ src/components/FinanceList/index.jsx
@@ -6,12 +6,15 @@ export const FinanceList = ({ listForm, id, removeInfo }) => {
     return (
         <section className={style.list__container}>
             <h2 className="title2">Resumo financeiro</h2>
+            {listForm.length > 0 ? (
             <ul>
                 {listForm.map((info) => {
                     const {title, value, enterExit} = info;
                     return <FinanceCard key={id} id={id} title={title} enterExit={enterExit} value={value} removeInfo={removeInfo} />                   
                 })}
             </ul>
+            ) : 
+            <p className="title3">Você ainda não possui nenhum lançamento</p>}
         </section>
     )
 }
